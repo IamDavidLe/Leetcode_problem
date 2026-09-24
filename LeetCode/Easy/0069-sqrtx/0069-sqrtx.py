69ae76b8fe59424f4951d69f4a0977b0288cdf21
Phat Le <legiaphat098@gmail.com>
2026-09-23 22:25:14 -0700

    Time: 1514 ms (8.57%), Space: 19.1 MB (87.77%) - LeetHub

---
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+
+        for i in range(x + 1):
+            if i * i > x :
+                return i -1
+            
+            elif i * i == x:
+                return i
+            
+
+        return -1
+            
