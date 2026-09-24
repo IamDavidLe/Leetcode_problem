34eadb4d98e394f8bb12d29b3f0b1150e884d613
Phat Le <legiaphat098@gmail.com>
2026-09-23 22:29:07 -0700

    Time: 4 ms (48.3%), Space: 19.2 MB (55.83%) - LeetHub

--- LeetCode/Easy/0069-sqrtx/0069-sqrtx.py
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx.py
@@ -1,15 +1,17 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+
         if x == 0 or x == 1:
             return x
-
-        for i in range(x + 1):
-            if i * i > x :
-                return i -1
-            
-            elif i * i == x:
-                return i
-            
-
-        return -1
-            
+        
+        while l <= r:
+            val = (l + r) // 2
+            if math.floor(val * val) > x:
+                r = val - 1
+            elif math.floor(val * val) < x:
+                l = val + 1
+            else:
+                return val
+                
+        return r
